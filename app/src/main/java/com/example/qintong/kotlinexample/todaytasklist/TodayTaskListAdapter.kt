8a027b739651ff58724c3de430e48d330b5bce83
qintong1 <qintong1@xiaomi.com>
2017-11-17 19:17:16 +0800

    modify item layout

--- app/src/main/java/com/example/qintong/kotlinexample/todaytasklist/TodayTaskListAdapter.kt
+++ app/src/main/java/com/example/qintong/kotlinexample/todaytasklist/TodayTaskListAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.qintong.kotlinexample.todaytasklist
 
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,25 +12,20 @@ import kotlin.properties.Delegates
 class TodayTaskListAdapter : RecyclerView.Adapter<TodayTaskListAdapter.TaskViewHolder>() {
     var tasksList: List<Task> by Delegates.observable(listOf()) {
         prop, old, new ->
-        Log.d("qintong", "change change change")
         notifyDataSetChanged()
-        Log.d("qintong", "change change change222")
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):TaskViewHolder {
-        Log.d("qintong", "onCreateViewHolder:" + viewType)
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.task_item, parent,
                 false)
         return TaskViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        Log.d("qintong", "onBindViewHolder:" + position)
         holder.name.setText(tasksList[position].title)
     }
 
     override fun getItemCount(): Int {
-        Log.d("qintong", "getItemCount:" + tasksList.size)
         return tasksList.size
     }
 
@@ -43,7 +37,6 @@ class TodayTaskListAdapter : RecyclerView.Adapter<TodayTaskListAdapter.TaskViewH
     }
 
     override fun getItemViewType(position: Int): Int {
-        Log.d("qintong", "getItemViewType:" + position)
         return 1
     }
 
